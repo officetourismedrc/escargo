704cfb60673039e497cc8b9340910fcb4427d63b
officetourismedrc <dmcont2023@gmail.com>
2025-08-08 14:28:10 +0100

    projets pages

--- resources/views/pages/nos-projets.blade.php
+++ resources/views/pages/nos-projets.blade.php
@@ -1,19 +1,327 @@
 <x-layouts.app :title="__('ok')">
 
-    <section class="section section-articles-top">
+   
+        <section class="section pj-section">
 
-        <div class="page-articles-container container">
-            <div class="page-articles-content content">
-                
+            <div class="container pj-container">
+                <div class="pj-header">
+                    <h1 class="pj-title">
+                        Nos Projets
+                    </h1>
+                </div>
+
+
+<!-- fin section -->
+
+
+                <div class="content pj-content">
+
+                    <div  class="pj-card">
+                        
+                        <div class="pj-card-content">
+                            <div class="pj-card-bg-img">
+                                 <img src="{{ asset('images/Nature.png')}}" alt="Projet 1" class="pj-card-img">
+                            </div>
+                            <div class="pj-link">
+                                <a href="#pj-d-card_01" class="pj-link-link">
+                                    <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                                </a>
+                            </div>
+
+                            <div class="pj-card-desc">
+                                <div class="pj-card-desc-content">
+                                <h2 class="pj-card-title">
+                                    Projet 1
+                                </h2>
+                                <p class="pj-card-text">
+                                    Description du projet 1. Ce projet est axé sur l'amélioration des performances et de l'efficacité.
+                                </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div  class="pj-card">
+                        
+                        <div class="pj-card-content">
+                            <div class="pj-card-bg-img">
+                                 <img src="{{ asset('images/Nature.png')}}" alt="Projet 1" class="pj-card-img">
+                            </div>
+                            <div class="pj-link">
+                                <a href="#pj-d-card_02" class="pj-link-link">
+                                    <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                                </a>
+                            </div>
+
+                            <div class="pj-card-desc">
+                                <div class="pj-card-desc-content">
+                                <h2 class="pj-card-title">
+                                    Projet 1
+                                </h2>
+                                <p class="pj-card-text">
+                                    Description du projet 1. Ce projet est axé sur l'amélioration des performances et de l'efficacité.
+                                </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div  class="pj-card">
+                        
+                        <div class="pj-card-content">
+                            <div class="pj-card-bg-img">
+                                 <img src="{{ asset('images/Nature.png')}}" alt="Projet 1" class="pj-card-img">
+                            </div>
+                            <div class="pj-link">
+                                <a href="#pj-d-card_03" class="pj-link-link">
+                                    <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                                </a>
+                            </div>
+
+                            <div class="pj-card-desc">
+                                <div class="pj-card-desc-content">
+                                <h2 class="pj-card-title">
+                                    Projet 1
+                                </h2>
+                                <p class="pj-card-text">
+                                    Description du projet 1. Ce projet est axé sur l'amélioration des performances et de l'efficacité.
+                                </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                     <div  class="pj-card">
+                        
+                        <div class="pj-card-content">
+                            <div class="pj-card-bg-img">
+                                 <img src="{{ asset('images/Nature.png')}}" alt="Projet 1" class="pj-card-img">
+                            </div>
+                            <div class="pj-link">
+                                <a href="" class="pj-link-link">
+                                    <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                                </a>
+                            </div>
+
+                            <div class="pj-card-desc">
+                                <div class="pj-card-desc-content">
+                                <h2 class="pj-card-title">
+                                    Projet 1
+                                </h2>
+                                <p class="pj-card-text">
+                                    Description du projet 1. Ce projet est axé sur l'amélioration des performances et de l'efficacité.
+                                </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                     <div  class="pj-card">
+                        
+                        <div class="pj-card-content">
+                            <div class="pj-card-bg-img">
+                                 <img src="{{ asset('images/Nature.png')}}" alt="Projet 1" class="pj-card-img">
+                            </div>
+                            <div class="pj-link">
+                                <a href="" class="pj-link-link">
+                                    <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                                </a>
+                            </div>
+
+                            <div class="pj-card-desc">
+                                <div class="pj-card-desc-content">
+                                <h2 class="pj-card-title">
+                                    Projet 1
+                                </h2>
+                                <p class="pj-card-text">
+                                    Description du projet 1. Ce projet est axé sur l'amélioration des performances et de l'efficacité.
+                                </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div  class="pj-card">
+                        
+                        <div class="pj-card-content">
+                            <div class="pj-card-bg-img">
+                                 <img src="{{ asset('images/Nature.png')}}" alt="Projet 1" class="pj-card-img">
+                            </div>
+                            <div class="pj-link">
+                                <a href="" class="pj-link-link">
+                                    <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                                </a>
+                            </div>
+
+                            <div class="pj-card-desc">
+                                <div class="pj-card-desc-content">
+                                <h2 class="pj-card-title">
+                                    Projet 1
+                                </h2>
+                                <p class="pj-card-text">
+                                    Description du projet 1. Ce projet est axé sur l'amélioration des performances et de l'efficacité.
+                                </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>   
+
             </div>
-        </div>
+        </section>
 
-    </section>
-    <section class="section section-articles-mid">
+        <section class="pj-d-section section">
+            <div class="pj-d-container">
+                <div class="pj-d-header container">
+                    <h1 class="pj-d-title">
+                        Détails du Projet
+                    </h1>
+                </div>
 
-    </section>
-    <section class="section section-articles-bottom">
+        
 
-    </section>
+                <div class="pj-d-content content">
+                    <div class="pj-d-card" id="pj-d-card_01">
+                       
+                        <div class="pj-d-card-content container">
+                            <div class="pj-d-card-bg-img">
+                                <img src="{{ asset('images/Nature.png')}}" alt="Projet 1" class="pj-d-card-img">
+                            </div>
+                            <div class="pj-d-card-desc">
+                                <h2 class="pj-d-card-title">Marina Port de plaisance</h2>
+                                <div class="pj-d-card-desc_content">
+                                    <div class="pj-d-card-desc-item">
+                                        <h3 class="pj-d-card-desc-item-title">Description</h3>
+                                        <p class="pj-d-card-desc-item-text">
+                                            Ce projet est axé sur l'amélioration des performances et de l'efficacité. Il comprend plusieurs phases de développement et de mise en œuvre.
+                                        </p>
+                                    </div>
+                                     <div class="pj-d-card-desc-item">
+                                    <div class="pj-d-card-desc-item-group">
+                                        <div class="pj-d-card-desc-item">
+                                            <h3 class="pj-d-card-desc-item-title">Technologies Utilisées</h3>
+                                            <ul class="pj-d-card-desc-item-lists">
+                                                <li class="pj-d-card-desc-item-list">HTML</li>
+                                                <li class="pj-d-card-desc-item-list">CSS</li>
+                                            </ul>
+                                        </div>
+                                        <div class="pj-d-card-desc-item">
+                                             <h3 class="pj-d-card-desc-item-title">Technologies Utilisées</h3>
+                                            <ul class="pj-d-card-desc-item-lists">
+                                                <li class="pj-d-card-desc-item-list">HTML</li>
+                                                <li class="pj-d-card-desc-item-list">CSS</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    </div>
+
+                                     <div class="pj-d-card-desc-item">
+                                        <h3 class="pj-d-card-desc-item-title">Description</h3>
+                                        <p class="pj-d-card-desc-item-text">
+                                            Ce projet est axé sur l'amélioration des performances et de l'efficacité. Il comprend plusieurs phases de développement et de mise en œuvre.
+                                        </p>
+                                     </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                     <div class="pj-d-card" id="pj-d-card_02">
+                        
+                        <div class="pj-d-card-content container">
+                            <div class="pj-d-card-bg-img">
+                                <img src="{{ asset('images/Nature.png')}}" alt="Projet 1" class="pj-d-card-img">
+                            </div>
+                            <div class="pj-d-card-desc">
+                                <h2 class="pj-d-card-title">Marina Port de plaisance</h2>
+                                <div class="pj-d-card-desc_content">
+                                    <div class="pj-d-card-desc-item">
+                                        <h3 class="pj-d-card-desc-item-title">Description</h3>
+                                        <p class="pj-d-card-desc-item-text">
+                                            Ce projet est axé sur l'amélioration des performances et de l'efficacité. Il comprend plusieurs phases de développement et de mise en œuvre.
+                                        </p>
+                                    </div>
+                                     <div class="pj-d-card-desc-item">
+                                    <div class="pj-d-card-desc-item-group">
+                                        <div class="pj-d-card-desc-item">
+                                            <h3 class="pj-d-card-desc-item-title">Technologies Utilisées</h3>
+                                            <ul class="pj-d-card-desc-item-lists">
+                                                <li class="pj-d-card-desc-item-list">HTML</li>
+                                                <li class="pj-d-card-desc-item-list">CSS</li>
+                                            </ul>
+                                        </div>
+                                        <div class="pj-d-card-desc-item">
+                                             <h3 class="pj-d-card-desc-item-title">Technologies Utilisées</h3>
+                                            <ul class="pj-d-card-desc-item-lists">
+                                                <li class="pj-d-card-desc-item-list">HTML</li>
+                                                <li class="pj-d-card-desc-item-list">CSS</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    </div>
+
+                                     <div class="pj-d-card-desc-item">
+                                        <h3 class="pj-d-card-desc-item-title">Description</h3>
+                                        <p class="pj-d-card-desc-item-text">
+                                            Ce projet est axé sur l'amélioration des performances et de l'efficacité. Il comprend plusieurs phases de développement et de mise en œuvre.
+                                        </p>
+                                     </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                     <div class="pj-d-card" id="pj-d-card_03">
+                       
+                        <div class="pj-d-card-content container">
+                            <div class="pj-d-card-bg-img">
+                                <img src="{{ asset('images/Nature.png')}}" alt="Projet 1" class="pj-d-card-img">
+                            </div>
+                            <div class="pj-d-card-desc">
+                                <h2 class="pj-d-card-title">Marina Port de plaisance</h2>
+                                <div class="pj-d-card-desc_content">
+                                    <div class="pj-d-card-desc-item">
+                                        <h3 class="pj-d-card-desc-item-title">Description</h3>
+                                        <p class="pj-d-card-desc-item-text">
+                                            Ce projet est axé sur l'amélioration des performances et de l'efficacité. Il comprend plusieurs phases de développement et de mise en œuvre.
+                                        </p>
+                                    </div>
+                                     <div class="pj-d-card-desc-item">
+                                    <div class="pj-d-card-desc-item-group">
+                                        <div class="pj-d-card-desc-item">
+                                            <h3 class="pj-d-card-desc-item-title">Technologies Utilisées</h3>
+                                            <ul class="pj-d-card-desc-item-lists">
+                                                <li class="pj-d-card-desc-item-list">HTML</li>
+                                                <li class="pj-d-card-desc-item-list">CSS</li>
+                                            </ul>
+                                        </div>
+                                        <div class="pj-d-card-desc-item">
+                                             <h3 class="pj-d-card-desc-item-title">Technologies Utilisées</h3>
+                                            <ul class="pj-d-card-desc-item-lists">
+                                                <li class="pj-d-card-desc-item-list">HTML</li>
+                                                <li class="pj-d-card-desc-item-list">CSS</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    </div>
+
+                                     <div class="pj-d-card-desc-item">
+                                        <h3 class="pj-d-card-desc-item-title">Description</h3>
+                                        <p class="pj-d-card-desc-item-text">
+                                            Ce projet est axé sur l'amélioration des performances et de l'efficacité. Il comprend plusieurs phases de développement et de mise en œuvre.
+                                        </p>
+                                     </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 
  </x-layouts.app>
